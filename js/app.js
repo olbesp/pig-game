48494e82bd933b8759bb5297caf88312267be52e
OlBesp <olbesp365@gmail.com>
2017-10-12 14:41:54 +0300

    Add 'New game' button functionality

--- js/app.js
+++ js/app.js
@@ -8,3 +8,47 @@ GAME RULES:
 - The first player who reach 100 points on GLOBAL score wins the game
 
 */
+var diceImgs = document.querySelectorAll(".dice");
+
+
+
+var scoreTotal1 = document.querySelector("#score-1");
+var scoreTotal2 = document.querySelector("#score-2");
+var roundScore1 = document.querySelector("#current-1");
+var roundScore2 = document.querySelector("#current-2");
+
+var activePlayer = 1;
+
+var diceScore = Math.floor(Math.random() * 12) + 1;
+
+document.querySelector("#current-" + activePlayer).textContent = diceScore;
+// Start new game
+function startGame() {
+  for (var i = 0; i < diceImgs.length; i++) {
+    diceImgs[i].style.display = "none";
+  }
+  scoreTotal1.textContent = 0;
+  scoreTotal2.textContent = 0;
+  roundScore1.textContent = 0;
+  roundScore2.textContent = 0;
+  activePlayer = 1;
+}
+
+// Hold current score
+function holdScore() {
+  if (activePlayer == 1) {
+    var sumOfRound = Number(scoreTotal1.textContent) + Number(roundScore1.textContent);
+    scoreTotal1.textContent = sumOfRound;
+    roundScore1.textContent = 0;
+    activePlayer = 2;
+  } else {
+    var sumOfRound = Number(scoreTotal2.textContent) + Number(roundScore2.textContent);
+    scoreTotal2.textContent = sumOfRound;
+    roundScore2.textContent = 0;
+    activePlayer = 1;
+  }
+}
+
+
+document.querySelector("#newGame").addEventListener("click", startGame);
+document.querySelector("#hold").addEventListener("click", holdScore);
